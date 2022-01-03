5b35562c8101f93959d2762072b3e8fd8661d08c
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-01-03 17:22:37 +0000

    Add function for buttons and basic table for game area

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
-function myFunction (event) {
-    console.log('You Pressed Play!');
-}
+// Wait for DOM content to load before beginning game.
+// Add event listeners to buttons.
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
 
-let myButton = document.getElementById('my-button');
-myButton.addEventListener('click', myFunction);
+    for (let button of buttons) {
+        button.addEventListener("click",function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Reset!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        }) 
+    }   
+})
+
+/**
+ * Gets the current amount of games won from the DOM and increments it by 1.  
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+}
