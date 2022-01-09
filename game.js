619356f877f6028b21b6076f927a83b439d8c2a5
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-01-09 19:00:16 +0000

    Add feedback and game play area

--- game.js
+++ game.js
@@ -2,23 +2,25 @@
 // Add event listeners to buttons.
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
+    	
+    	for (let button of buttons) {
+    	    button.addEventListener("click",function() {
+    	        if (this.getAttribute("data-type") === "submit") {
+    	            alert("You clicked Reset!");
+    	        } else if (this.getAttribute("data-type") === "play") {
+    	            alert(`You clicked ${gameType}`);
+	            } else {
+	                let gameType = this.getAttribute("data-type");
+	                alert(`You clicked ${gameType}`);	
+	         }
+        })
+	}
+});
 
-    for (let button of buttons) {
-        button.addEventListener("click",function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Reset!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
-        }) 
-    }   
-})
-
-/**
- * Gets the current amount of games won from the DOM and increments it by 1.  
- */
-function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-}
+                /**
+                 * Gets the current amount of games won from the DOM and increments it by 1.  
+                 */
+                function incrementScore() {
+                    let oldScore = parseInt(document.getElementById("score").innerText);
+                    document.getElementById("score").innerText = ++oldScore;
+                };
