c4a417f9fa1788c532d4ea7ec4f82880e6e0c277
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-01-11 18:06:54 +0000

    Add JS

--- game.js
+++ game.js
@@ -17,10 +17,95 @@ document.addEventListener("DOMContentLoaded", function(){
 	}
 });
 
-                /**
-                 * Gets the current amount of games won from the DOM and increments it by 1.  
-                 */
-                function incrementScore() {
-                    let oldScore = parseInt(document.getElementById("score").innerText);
-                    document.getElementById("score").innerText = ++oldScore;
-                };
+//Event listeners for Game screen
+// document.getElementsByTagName("td").forEach(box() => box.addEventListener('click', playAvailable) );
+document.addEventListener("DOMContentLoaded", function(){
+    let box = document.getElementsByTagName("td");
+
+	for (let box of boxes) {
+		box.addEventListener("click",function() {
+			if (this.getAttribute(playAvailable) === true) {
+				alert("You made your move!");
+			} else {
+				let boxSelected = this.getAttribute(playAvailable);
+				alert(`${boxSelected} has already been slected`);	
+		 	}
+		})
+	}
+}) ;
+
+
+/**
+ * Status of the game
+ */
+ const gameStatus = document.querySelector('.playerstatus');
+
+/**
+ * This function will stop the game when it is no longer playable
+ */
+ let gameActive = true; 
+
+document.querySelector('.reset').addEventListener('click', playAgain);
+
+/**
+ * Function used to determine what cells in the table have been played
+ * This will help determine gameActive and what message should be displayed
+ * Either win, loss or tie
+ */
+ let gamePlay = ["","","","","","","","",""]
+
+ const winText = () => `Player ${currentPlayer} Wins!!`;
+ const drawText = () => `The Game is a draw, try again!`;
+ 
+
+/**
+ * Function for who takes next turn
+ */
+let currentPlayer = "X";
+
+const currentPlayersGo  = () => `Player ${currentPlayer}'s turn to play!`;
+
+// Functions needed for game play!
+
+gameStatus.innerHTML = currentPlayersGo();
+function boxSelected() {
+
+}
+
+function nextTurn() {
+	// if (currentPlayer === "X") {
+	// 	currentPlayer = `Its's ${currentPlayersGo} turn`;
+	// } else {
+	// 	result = `Its's ${currentPlayersGo} turn`;
+	// }
+	// return currentPlayer;
+};
+
+/**
+ * When game is over it will go back to default settings.
+ */
+function playAgain() {
+	// currentPlayer = "X";
+	// gameActive = true;
+	// gamePlay = ["","","","","","","","",""];
+
+	// gameActive.innerHTML = currentPlayersGo;
+	// document.getElementsByClassName('.box').forEach(box => box.innerHTML = "");
+}
+
+function gameScore() {
+
+}
+
+function playAvailable(playAvailableValid) {
+	const boxChosen = playAvailableValid.target;
+	const 
+}
+
+/**
+* Gets the current amount of games won from the DOM and increments it by 1. 
+*/
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+ };
